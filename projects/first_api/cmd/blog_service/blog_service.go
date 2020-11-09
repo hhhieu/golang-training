c3dd7870f8f4d02055ec691f1827a7a20aedacbc
hhhieu <hoanghonghieu@gmail.com>
2020-11-09 16:06:20 +0700

    Connect to database base on the configuration file.

--- projects/first_api/cmd/blog_service/blog_service.go
+++ projects/first_api/cmd/blog_service/blog_service.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"log"
-	"time"
+
+	"github.com/hhhieu/golang-training/first_api/pkg/database"
+	"github.com/hhhieu/golang-training/first_api/pkg/system"
+
+	"github.com/hhhieu/golang-training/first_api/internal/pkg/config"
 )
 
+const configFile = "/home/configs/blog_service.yml"
+
 func main() {
-	log.Println("HelloWorld")
-	for true {
-		time.Sleep(5 * time.Millisecond)
+	c, err := config.LoadConfig(system.IOUtil{}, configFile)
+	if err != nil {
+		log.Fatalf("Loading configuration failed: %v", err)
+	}
+	p, err := database.NewProvider(c.Database)
+	if err != nil {
+		log.Fatalf("Creating provider failed: %v", err)
+	}
+	err = p.Open()
+	if err != nil {
+		log.Fatalf("Connecting database failed: %v", err)
 	}
 }
