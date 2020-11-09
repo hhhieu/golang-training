4bfd109b520825360884b4f062e22824d265fe14
hhhieu <hoanghonghieu@gmail.com>
2020-11-09 16:04:07 +0700

    Load the service configuration from file, which is generated by confd.

---
+++ projects/first_api/internal/pkg/config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/hhhieu/golang-training/first_api/pkg/database"
+	"github.com/hhhieu/golang-training/first_api/pkg/system"
+	"gopkg.in/yaml.v2"
+)
+
+const defaultURL = "localhost:3306"
+
+// Config contains the data source name(DSN)
+type Config struct {
+	Database database.Config
+}
+
+// LoadConfig load the configuration from file
+func LoadConfig(ioUtil system.IOUtiler, filePath string) (*Config, error) {
+	// Read the configuration file
+	configBytes, err := ioUtil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+	// Parse the yaml data to struct
+	config := Config{
+		Database: database.Config{
+			URL: defaultURL,
+		},
+	}
+	yaml.Unmarshal(configBytes, &config)
+	// Validate the database configuration
+	if err := config.Database.Validate(); err != nil {
+		return nil, fmt.Errorf("Loading database configuration failed: %v", err)
+	}
+	return &config, nil
+}
