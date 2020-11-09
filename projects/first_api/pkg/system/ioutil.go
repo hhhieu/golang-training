f2e49a2237dd9e3a281462a4d08bbb276115079c
hhhieu <hoanghonghieu@gmail.com>
2020-11-09 15:58:36 +0700

    Create a file system wrapper

---
+++ projects/first_api/pkg/system/ioutil.go
@@ -0,0 +1,14 @@
+package system
+
+// IOUtiler is interface of io util classes
+type IOUtiler interface {
+	ReadFile(fileName string) ([]byte, error)
+}
+
+// IOUtil is a wrapper of io/ioutil
+type IOUtil struct{}
+
+// ReadFile opens file and reads all contents in that file.
+func (u IOUtil) ReadFile(fileName string) ([]byte, error) {
+	return u.ReadFile(fileName)
+}
